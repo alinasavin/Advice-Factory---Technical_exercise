f03543ae3684d4b0e7a9a08e185132d8d410a5f6
Alina Savin <81432036+alinasavin@users.noreply.github.com>
2021-10-13 23:15:14 +0300

    fetched data from firebase

--- advice-app/src/pages/ShowAdvice.js
+++ advice-app/src/pages/ShowAdvice.js
@@ -1,7 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import fireDb from '../firebase';
+import { Link } from 'react-router-dom';
+import './ShowAdvice.scss';
 
 const ShowAdvice = () => {
-  return <div>Advice List</div>;
+  const [data, setData] = useState({});
+  // query to get data from the firebase database
+  useEffect(() => {
+    fireDb.child('advice-list').on('value', (snapshot) => {
+      if (snapshot.val() !== null) {
+        setData({ ...snapshot.val() });
+      } else {
+        setData({});
+      }
+    });
+
+    return () => {
+      setData({});
+    };
+  }, []);
+
+  return <div className="listContainer">Advice List</div>;
 };
 
 export default ShowAdvice;

1c4933b74dbe212079f9f1e96dcbdccb5d90fdf1
Alina Savin <81432036+alinasavin@users.noreply.github.com>
2021-10-14 13:00:10 +0300

    added readme

---
+++ README.md
@@ -0,0 +1,77 @@
+# Advice Factory
+
+## About the project
+
+After an amazing first chat with Amigo I was given the task to create a random quote inspirational quote generator that allows the user to view random quotes and add new quotes.
+
+## Overview
+
+The advice app allows the user to generate random pieces of advice with the touch of the button. The user can also type in his own advice that gets saved into the firebase database where can be edited in realtime. As a bonus the app give some extra funny quotes to lighten the user's mood, all stored locally in the app.
+
+### Built with
+
+- React
+- Sass
+- Firebase
+- Jest
+- [API's ](https://api.adviceslip.com/)
+- Netlify hosting
+
+### Prerequisites
+
+- npm
+  npm install | npm@latest -g
+- Ensure correct folder is selected, i.e. cd advice-app
+
+### Installation
+
+1. Get Firebase configuration at https://firebase.google.com
+
+2. Clone the project
+
+```bash
+  git clone https://github.com/alinasavin/Technical_exercise.git
+```
+
+4. Go to the project directory
+
+```bash
+  cd advice-app
+```
+
+5. Install dependencies
+
+```bash
+  npm install
+```
+
+6. Start the server
+
+```bash
+  npm run start
+```
+
+7. Enter your configuration key in firebase.js
+
+### Dependencies
+
+**App:** react, react-toastify, react-dom, react-router-dom, react-scripts, react-icons, node-sass  
+**Testing:** react-testing-library, Jest
+
+## Running Tests
+
+To run the unit tests (utilising Jest and react-testing-library), enter the following command:
+
+```bash
+    npm test
+```
+
+## What I have learned
+
+This was a great opportunity for me to refresh my React skills and the use of React hooks. It was also a challenge as I have not worked before with Firebase Realtime database. I have read up on the resources available and overcame some obstacles with the new version of firebase used, but all worked in the end.
+
+## Future goals
+
+Learn and build this using MondoDb or another data database program.
+
+## Created by Alina Savin
--- advice-app/src/data.js
+++ advice-app/src/data.js
@@ -13,50 +13,48 @@ export const quotes = [
   },
   {
     id: 3,
-    quote:
-      'When life gives you lemons you tell life to get a life because lemons are a terrible gift.',
-    author: 'Patrick Schell',
+    quote: 'When life gives you lemons, squirt someone in the eye.',
+    author: 'Cathy Guiswite',
   },
   {
     id: 4,
-    quote:
-      'When life gives you lemons you tell life to get a life because lemons are a terrible gift.',
+    quote: 'When life gives you lemons, order the lobster tail.',
     author: 'Patrick Schell',
   },
   {
     id: 5,
     quote:
       'When life gives you lemons you tell life to get a life because lemons are a terrible gift.',
-    author: 'Patrick Schell',
+    author: 'Ziad K. Abdelnour',
   },
   {
     id: 6,
     quote:
-      'When life gives you lemons you tell life to get a life because lemons are a terrible gift.',
-    author: 'Patrick Schell',
+      "When life gives you lemons, don't make lemonade! Make life take the lemons back... What am I supposed to do with these? I demand to see life's manager...",
+    author: 'J. K. Simmons',
   },
   {
     id: 7,
     quote:
-      'When life gives you lemons you tell life to get a life because lemons are a terrible gift.',
-    author: 'Patrick Schell',
+      'When life gives you lemons, you exchange them at the store for something more edible.',
+    author: 'Grace Helbig',
   },
   {
     id: 8,
     quote:
-      'When life gives you lemons you tell life to get a life because lemons are a terrible gift.',
-    author: 'Patrick Schell',
+      'The old saying is that when life gives you lemons, make lemonade. I say forget that. When life gives you lemons, make margaritas.',
+    author: 'Kristin Neff',
   },
   {
     id: 9,
     quote:
-      'When life gives you lemons you tell life to get a life because lemons are a terrible gift.',
-    author: 'Patrick Schell',
+      'A grapefruit is just a lemon that saw an opportunity and took advantage of it.',
+    author: 'Oscar Wilde',
   },
   {
     id: 10,
     quote:
-      'When life gives you lemons you tell life to get a life because lemons are a terrible gift.',
-    author: 'Patrick Schell',
+      'Life handed me lemons, so I made lemonade, lemon cake, lemon drops, and I even used the rinds to make lemon art. So the joke is on you, life...',
+    author: 'Emilyann Allen',
   },
 ];
